10ec6d4422528add864c6875f53346830bab9d2a
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-03-10 16:18:17 +0300

    Добавил настройки подключения к БД

--- fitfriends-users/src/app.module.ts
+++ fitfriends-users/src/app.module.ts
@@ -1,9 +1,25 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {MongooseModule} from '@nestjs/mongoose';
+import {ENV_FILE_PATH} from './app.constant';
 import {AuthModule} from './auth/auth.module';
+import databaseConfig, {getMongoDbConfig} from './config/database.config';
 import {UsersModule} from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig]
+    }),
+    MongooseModule.forRootAsync(
+      getMongoDbConfig()
+    ),
+    AuthModule,
+    UsersModule
+  ],
   controllers: [],
   providers: []
 })
