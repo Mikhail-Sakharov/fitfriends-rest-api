42c59bac78a18a287ee87b2379c4bde4c49330b9
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-03-16 19:38:00 +0300

    FF-service: добавил интерфейс тренировки и вспомогательные типы

---
+++ fitfriends-sevice/src/types/training-gender.enum.ts
@@ -0,0 +1,5 @@
+export enum TrainingGenderType {
+  ForMale = 'для мужчин',
+  ForFemale = 'для женщин',
+  ForEverybody = 'для всех'
+}
---
+++ fitfriends-sevice/src/types/training.interface.ts
@@ -0,0 +1,22 @@
+import {Duration} from './duration.enum';
+import {TrainingGenderType} from './training-gender.enum';
+import {TrainingLevel} from './training-level.enum';
+import {TrainingType} from './training-type.enum';
+
+export interface Training {
+  _id?: string;
+  createdAt?: string;
+  title: string;
+  bgImageUrl: string;
+  level: TrainingLevel;
+  type: TrainingType;
+  duration: Duration;
+  price: number;
+  caloriesCount: number;
+  description: string;
+  gender: TrainingGenderType;
+  videoUrl: string;
+  rating?: number;
+  coachId: string;
+  isSpecialOffer?: boolean;
+}
