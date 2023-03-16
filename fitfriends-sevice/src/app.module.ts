9c53ce8449f727aad07aef381d2d263dac8400f7
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-03-16 17:43:33 +0300

    FF-service: подключил ConfigModule

--- fitfriends-sevice/src/app.module.ts
+++ fitfriends-sevice/src/app.module.ts
@@ -1,8 +1,18 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {ENV_FILE_PATH} from './app.constant';
 import {TrainingsModule} from './trainings/trainings.module';
 
 @Module({
-  imports: [TrainingsModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: []
+    }),
+    TrainingsModule
+  ],
   controllers: [],
   providers: []
 })
