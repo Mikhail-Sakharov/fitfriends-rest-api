0106b1d74292674a66d268134d19d4f4d4e48d2b
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-04 21:36:38 +0300

    FF-notifier: настроил запуск сервиса

--- fitfriends-notifier/src/app.module.ts
+++ fitfriends-notifier/src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {ENV_FILE_PATH} from './app.constant';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: []
+    }),
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
--- fitfriends-notifier/src/main.ts
+++ fitfriends-notifier/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {Logger, ValidationPipe} from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {cors: true});
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true
+    })
+  );
+
+  const port = process.env.PORT || 3003;
+  await app.listen(port);
+  Logger.log(
+    `🚀 The "fitfriends-notifier" is running on: http://localhost:${port}`
+  );
 }
 bootstrap();
