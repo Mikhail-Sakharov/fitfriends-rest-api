bd6dca825bcd3b733fc7536752cb9e6933596de4
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-04 21:48:40 +0300

    FF-notifier: поднял контейнер с БД, подключил БД к сервису

--- fitfriends-notifier/src/app.module.ts
+++ fitfriends-notifier/src/app.module.ts
@@ -1,6 +1,8 @@
 import {Module} from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
 import {ENV_FILE_PATH} from './app.constant';
+import {MongooseModule} from '@nestjs/mongoose';
+import databaseConfig, {getMongoDbConfig} from './config/database.config';
 
 @Module({
   imports: [
@@ -8,8 +10,11 @@ import {ENV_FILE_PATH} from './app.constant';
       cache: true,
       isGlobal: true,
       envFilePath: ENV_FILE_PATH,
-      load: []
+      load: [databaseConfig]
     }),
+    MongooseModule.forRootAsync(
+      getMongoDbConfig()
+    ),
   ],
   controllers: [],
   providers: []
