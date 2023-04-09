6b7a936cd98648b905fd87d9d4989fce8773c956
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-09 21:47:38 +0300

    FF-notifier/mail: добавил методы отправки уведомлений при подписке и отписке на обновления пользователя

---
+++ fitfriends-notifier/src/types/add-subscriber-email-data.interface.ts
@@ -0,0 +1,5 @@
+export interface AddSubscriberEmailData {
+  sendTo: string;
+  suscriberName: string;
+  coachName: string;
+}
---
+++ fitfriends-notifier/src/types/remove-suscriber-email-data.interface.ts
@@ -0,0 +1,5 @@
+export interface RemoveSubscriberEmailData {
+  sendTo: string;
+  suscriberName: string;
+  coachName: string;
+}
