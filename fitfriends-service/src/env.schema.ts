91d80b1ad21c4e2bd2423e11e07aa46f10e9d7a9
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-10 16:49:55 +0300

    FF-service/trainings&FF-notifier: добавил переменные окружения для связи двух МС через RabbitMQ

--- fitfriends-service/src/env.schema.ts
+++ fitfriends-service/src/env.schema.ts
@@ -3,6 +3,7 @@ import * as Joi from 'joi';
 const DEFAULT_MONGO_DB_HOST = 'localhost';
 const DEFAULT_MONGO_DB_PORT = 27019;
 const DEFAULT_UPLOAD_DIRECORY = './files';
+const DEFAULT_RABBIT_HOST = 'localhost:5672';
 
 export default Joi.object({
   PORT: Joi.number().port(),
@@ -13,5 +14,9 @@ export default Joi.object({
   MONGO_PASSWORD: Joi.string().required(),
   MONGO_AUTH_BASE: Joi.string().required(),
   ACCESS_TOKEN_SECRET: Joi.string().required(),
-  UPLOAD_DIRECORY: Joi.string().default(DEFAULT_UPLOAD_DIRECORY).required()
+  UPLOAD_DIRECORY: Joi.string().default(DEFAULT_UPLOAD_DIRECORY).required(),
+  RABBIT_HOST: Joi.string().default(DEFAULT_RABBIT_HOST).required(),
+  RABBIT_USER: Joi.string().required(),
+  RABBIT_PASSWORD: Joi.string().required(),
+  RABBIT_TRAININGS_SERVICE_QUEUE: Joi.string().required(),
 });
