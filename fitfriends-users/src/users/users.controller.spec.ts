af43fa890d579b03578286a7a36ddbe28bc14fb4
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-14 19:42:00 +0300

    UsersController: отредавтировал unit тест

--- fitfriends-users/src/users/users.controller.spec.ts
+++ fitfriends-users/src/users/users.controller.spec.ts
@@ -1,18 +1,46 @@
 import {Test, TestingModule} from '@nestjs/testing';
 import {UsersController} from './users.controller';
+import {UsersService} from './users.service';
+import {RABBITMQ_SERVICE} from 'src/app.constant';
+import {ConfigService} from '@nestjs/config';
 
-describe('UsersController', () => {
-  let controller: UsersController;
+describe('AuthController', () => {
+  let usersController: UsersController;
+  const ApiServiceProvider = {
+    provide: UsersService,
+    useFactory: () => ({
+      getFriends: jest.fn(),
+      addFriend: jest.fn(),
+      removeFriend: jest.fn(),
+      getUsers: jest.fn(),
+      getUser: jest.fn(),
+      updateUser: jest.fn(),
+      setAvatarPath: jest.fn(),
+      setCertificateFilePath: jest.fn()
+    })
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
+    const moduleRef: TestingModule = await Test.createTestingModule({
+        controllers: [UsersController],
+        providers: [
+          UsersService,
+          ConfigService,
+          ApiServiceProvider,
+          {
+            provide: RABBITMQ_SERVICE,
+            useValue: {
+              getResult: jest.fn(),
+            },
+          }
+        ]
+      })
+      .compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = moduleRef.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
   });
 });
