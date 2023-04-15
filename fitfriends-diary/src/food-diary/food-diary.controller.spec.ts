15f6aace0c0f6155026a4ddf2932cf6f442ec57c
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-15 20:40:25 +0300

    FF-diary: добавил тесты для контроллер в модуле food-diary

---
+++ fitfriends-diary/src/food-diary/food-diary.controller.spec.ts
@@ -0,0 +1,104 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {UserRole} from 'src/types/user-role.enum';
+import {FoodDiaryController} from './food-diary.controller';
+import {FoodDiaryService} from './food-diary.service';
+import {CreateFoodDiaryDto} from 'src/dto/create-food-diary.dto';
+import {UpdateFoodDiaryDto} from 'src/dto/update-food-diary.dto';
+
+describe('TrainingsDiaryController', () => {
+  let foodDiaryController: FoodDiaryController;
+  let foodDiaryService: FoodDiaryService;
+  const ApiServiceProvider = {
+    provide: FoodDiaryService,
+    useFactory: () => ({
+      createFoodDiary: jest.fn(),
+      getFoodDiaries: jest.fn(),
+      showFoodDiary: jest.fn(),
+      updateFoodDiary: jest.fn(),
+      deleteFoodDiary: jest.fn()
+    })
+  };
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+        controllers: [FoodDiaryController],
+        providers: [
+          FoodDiaryService,
+          ApiServiceProvider
+        ]
+      })
+      .compile();
+
+    foodDiaryController = moduleRef.get<FoodDiaryController>(FoodDiaryController);
+    foodDiaryService = moduleRef.get<FoodDiaryService>(FoodDiaryService);
+  });
+
+  it('should be defined', () => {
+    expect(foodDiaryController).toBeDefined();
+  });
+
+  it("calling createFoodDiary method", () => {
+    const dto = new CreateFoodDiaryDto();
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    foodDiaryController.createFoodDiary(dto, req);
+    expect(foodDiaryService.createFoodDiary).toHaveBeenCalled();
+  });
+
+  it("calling getFoodDiaries method", () => {
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    foodDiaryController.getFoodDiaries(req);
+    expect(foodDiaryService.getFoodDiaries).toHaveBeenCalled();
+  });
+
+  it("calling showFoodDiary method", () => {
+    const id = '';
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    foodDiaryController.showFoodDiary(id, req);
+    expect(foodDiaryService.showFoodDiary).toHaveBeenCalled();
+  });
+
+  it("calling updateFoodDiary method", () => {
+    const id = '';
+    const dto = new UpdateFoodDiaryDto();
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    foodDiaryController.updateFoodDiary(id, dto, req);
+    expect(foodDiaryService.updateFoodDiary).toHaveBeenCalled();
+  });
+
+  it("calling deleteFoodDiary method", () => {
+    const id = '';
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    foodDiaryController.deleteFoodDiary(id, req);
+    expect(foodDiaryService.deleteFoodDiary).toHaveBeenCalled();
+  });
+});
