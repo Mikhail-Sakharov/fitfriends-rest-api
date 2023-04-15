8d86393e5933893437a85eb11c753a52b83beaa5
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-15 19:57:52 +0300

    FF-sevice: добавил тесты для контроллера в модуле gyms

---
+++ fitfriends-service/src/gyms/gyms.controller.spec.ts
@@ -0,0 +1,92 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {GymsController} from './gyms.controller';
+import {GymsService} from './gyms.service';
+import {UserRole} from 'src/types/user-role.enum';
+
+describe('TrainingsController', () => {
+  let gymsController: GymsController;
+  let gymsService: GymsService;
+  const ApiServiceProvider = {
+    provide: GymsService,
+    useFactory: () => ({
+      createGym: jest.fn(),
+      getGyms: jest.fn(),
+      getCatalog: jest.fn(),
+      addGymToFavorites: jest.fn(),
+      removeGymFromFavorites: jest.fn(),
+      getFavoriteGyms: jest.fn(),
+      seed: jest.fn()
+    })
+  };
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+        controllers: [GymsController],
+        providers: [
+          GymsService,
+          ApiServiceProvider
+        ]
+      })
+      .compile();
+
+    gymsController = moduleRef.get<GymsController>(GymsController);
+    gymsService = moduleRef.get<GymsService>(GymsService);
+  });
+
+  it('should be defined', () => {
+    expect(gymsController).toBeDefined();
+  });
+
+  it("calling getGyms method", () => {
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+    const query = {
+      minPrice: 1000
+    };
+
+    gymsController.getGyms(query, req);
+    expect(gymsService.getCatalog).toHaveBeenCalled();
+  });
+
+  it("calling getFavoriteGyms method", () => {
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    gymsController.getFavoriteGyms(req);
+    expect(gymsService.getFavoriteGyms).toHaveBeenCalled();
+  });
+
+  it("calling addGymToFavorites method", () => {
+    const id = '';
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    gymsController.addGymToFavorites(id, req);
+    expect(gymsService.addGymToFavorites).toHaveBeenCalled();
+  });
+
+  it("calling removeGymFromFavorites method", () => {
+    const id = '';
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    gymsController.removeGymFromFavorites(id, req);
+    expect(gymsService.removeGymFromFavorites).toHaveBeenCalled();
+  });
+});
