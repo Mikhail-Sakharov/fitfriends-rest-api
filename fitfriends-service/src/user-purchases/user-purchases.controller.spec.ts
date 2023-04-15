6d888b58d537a2035d3d7a4c26b6049649c8379d
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-15 18:58:09 +0300

    FF-sevice: добавил тесты для контроллера в модуле user-purchases

---
+++ fitfriends-service/src/user-purchases/user-purchases.controller.spec.ts
@@ -0,0 +1,114 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {UserPurchasesController} from './user-purchases.controller';
+import {UserPurchasesService} from './user-purchases.service';
+import {UserRole} from 'src/types/user-role.enum';
+
+describe('UserPurchasesController', () => {
+  let userPurchasesController: UserPurchasesController;
+  let userPurchasesService: UserPurchasesService;
+  const ApiServiceProvider = {
+    provide: UserPurchasesService,
+    useFactory: () => ({
+      getPurchases: jest.fn(),
+      getBalance: jest.fn(),
+      incrementTrainingsCount: jest.fn(),
+      incrementGymsCount: jest.fn(),
+      decrementTrainingsCount: jest.fn(),
+      decrementGymsCount: jest.fn()
+    })
+  };
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+        controllers: [UserPurchasesController],
+        providers: [
+          UserPurchasesService,
+          ApiServiceProvider
+        ]
+      })
+      .compile();
+
+    userPurchasesController = moduleRef.get<UserPurchasesController>(UserPurchasesController);
+    userPurchasesService = moduleRef.get<UserPurchasesService>(UserPurchasesService);
+  });
+
+  it('should be defined', () => {
+    expect(userPurchasesController).toBeDefined();
+  }); 
+
+  it("calling getPurchases method", () => {
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    userPurchasesController.getPurchases(req);
+    expect(userPurchasesService.getPurchases).toHaveBeenCalled();
+  });
+
+  it("calling getBalance method", () => {
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    userPurchasesController.getBalance(req);
+    expect(userPurchasesService.getBalance).toHaveBeenCalled();
+  });
+
+  it("calling incrementTrainingsCount method", () => {
+    const id = '';
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    userPurchasesController.incrementTrainingsCount(id, req);
+    expect(userPurchasesService.incrementTrainingsCount).toHaveBeenCalled();
+  });
+
+  it("calling incrementGymsCount method", () => {
+    const id = '';
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    userPurchasesController.incrementGymsCount(id, req);
+    expect(userPurchasesService.incrementGymsCount).toHaveBeenCalled();
+  });
+
+  it("calling decrementTrainingsCount method", () => {
+    const id = '';
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    userPurchasesController.decrementTrainingsCount(id, req);
+    expect(userPurchasesService.decrementTrainingsCount).toHaveBeenCalled();
+  });
+  
+  it("calling decrementGymsCount method", () => {
+    const id = '';
+    const req = {user: {
+      sub: '',
+      userName: '',
+      userRole: UserRole.User,
+      email: ''
+    }};
+
+    userPurchasesController.decrementGymsCount(id, req);
+    expect(userPurchasesService.decrementGymsCount).toHaveBeenCalled();
+  });
+});
